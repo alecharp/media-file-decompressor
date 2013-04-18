44aa33c68e8f1e83928dcd22eb895b1de7c68109
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2013-04-18 23:27:34 +0200

    Decompression architecture setting
    The implementation to use is chose based on the file extension file and a
    properties setting. The properties file must set the extension = class.
    The class to use must implement Decompression interface.

---
+++ decompressor-core/src/test/java/org/lecharpentier/media/decompressor/core/extraction/DecompressionManagerTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 Adrien Lecharpentier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lecharpentier.media.decompressor.core.extraction;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
+ */
+public class DecompressionManagerTest {
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void unsupportedExtension() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        DecompressionManager.getInstance().getDecompressionImplForFile("a.lecharp");
+    }
+
+    @Test(expected = ClassNotFoundException.class)
+    public void classNotFoundForExtension() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        DecompressionManager.getInstance().getDecompressionImplForFile("a.test");
+    }
+
+    @Test(expected = IncorrectImplementationException.class)
+    public void invalidClassForExtension() throws IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        DecompressionManager.getInstance().getDecompressionImplForFile("a.drien");
+    }
+}
