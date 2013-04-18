0092c81d2c4d59853d092e800edef7faa180c081
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2013-04-18 22:31:12 +0200

    Browse the sfv file content line by line

---
+++ decompressor-core/src/main/java/org/lecharpentier/media/decompressor/core/utils/SFVIterator.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2013 Adrien Lecharpentier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lecharpentier.media.decompressor.core.utils;
+
+import org.lecharpentier.media.decompressor.core.model.ArchiveResource;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
+ */
+public class SFVIterator implements Iterator<ArchiveResource> {
+
+    private final Scanner scanner;
+    private final Pattern pattern;
+
+    public SFVIterator(String sfvFilePath) throws FileNotFoundException {
+        scanner = new Scanner(new FileInputStream(sfvFilePath));
+        pattern = Pattern.compile("^([^ ]+) ([^ ]{8})$");
+    }
+
+    @Override
+    public boolean hasNext() {
+        return scanner.hasNextLine();
+    }
+
+    @Override
+    public ArchiveResource next() {
+        String s1 = scanner.nextLine();
+        Matcher matcher = pattern.matcher(s1);
+        if (!matcher.matches()) {
+            throw new IllegalStateException(String.format("The current line (%s) mismatch the expected format.", s1));
+        }
+        return new ArchiveResource(matcher.group(1), matcher.group(2));
+    }
+
+    /**
+     * This method does nothing in this implementation
+     */
+    @Override
+    public void remove() {}
+}
---
+++ decompressor-core/src/test/java/org/lecharpentier/media/decompressor/core/utils/SFVIteratorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Adrien Lecharpentier
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lecharpentier.media.decompressor.core.utils;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
+ */
+public class SFVIteratorTest {
+
+    @Test
+    public void validSFVFile() throws FileNotFoundException {
+        SFVIterator sfvIterator = getSfvIterator("media.sfv");
+        int cpt = 0;
+        while (sfvIterator.hasNext()) { sfvIterator.next(); cpt += 1; }
+        assertThat(cpt).isEqualTo(3);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void invalidSFVFile() throws FileNotFoundException {
+        SFVIterator sfvIterator = getSfvIterator("bad-media.sfv");
+        while (sfvIterator.hasNext()) { sfvIterator.next(); }
+    }
+
+    private SFVIterator getSfvIterator(String fileName) throws FileNotFoundException {
+        return new SFVIterator(
+                SFVIteratorTest.class.getResource(
+                        SFVIteratorTest.class.getSimpleName() + File.separator + fileName
+                ).getPath()
+        );
+    }
+}
