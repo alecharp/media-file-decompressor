4a436d7bf8fd5952ddece26336653cb451adc8f7
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2013-05-14 19:22:40 +0200

    Setup RAR extraction

--- decompressor-rar-impl/src/main/java/org/lecharpentier/media/decompressor/impl/extraction/DecompressorRarImpl.java
+++ decompressor-rar-impl/src/main/java/org/lecharpentier/media/decompressor/impl/extraction/DecompressorRarImpl.java
@@ -16,15 +16,21 @@
 
 package org.lecharpentier.media.decompressor.impl.extraction;
 
+import com.github.junrar.extract.ExtractArchive;
 import org.lecharpentier.media.decompressor.core.extraction.Decompressor;
 import org.lecharpentier.media.decompressor.core.extraction.ExtractionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
  */
 public class DecompressorRarImpl implements Decompressor {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DecompressorRarImpl.class);
 
     @Override
     public String getExtension() {
@@ -33,5 +39,48 @@ public class DecompressorRarImpl implements Decompressor {
 
     @Override
     public void decompress(File archiveFile) throws ExtractionException {
+        verifyFileExists(archiveFile);
+        File file = normalizeFile(archiveFile);
+        File destination = file.getParentFile();
+        extract(file, destination);
+    }
+
+    /**
+     * Checks that the given file actually exists
+     *
+     * @param archiveFile The file to check
+     * @throws ExtractionException Thrown if the file is null or does not exist
+     */
+    private void verifyFileExists(File archiveFile) throws ExtractionException {
+        if (archiveFile == null || !archiveFile.exists()) {
+            throw new ExtractionException("The file does not exist: " + archiveFile.getPath());
+        }
+    }
+
+    /**
+     * Resolves the canonical path to the file, removing any unneccessary indirections.
+     *
+     * @param archiveFile The file to resolve
+     * @return A new File with a normalized path
+     * @throws ExtractionException If the path cannot be resolved
+     */
+    private File normalizeFile(File archiveFile) throws ExtractionException {
+        try {
+            return archiveFile.getCanonicalFile();
+        } catch (IOException e) {
+            throw new ExtractionException("Unable to resolve canonical path to " + archiveFile);
+        }
+    }
+
+    /**
+     * Proceed to the extraction of the file into the destination folder
+     *
+     * @param archive     the RAR file to extract
+     * @param destination the folder within to extract the RAR
+     * @throws ExtractionException If the archive cannot be decompressed
+     */
+    private void extract(File archive, File destination) throws ExtractionException {
+        ExtractArchive extractArchive = new ExtractArchive();
+        extractArchive.extractArchive(archive, destination);
     }
 }
